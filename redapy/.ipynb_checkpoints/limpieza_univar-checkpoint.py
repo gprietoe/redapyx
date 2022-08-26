93eb8f01d6ce85e38c631b79a73ea488ba1c6aaf
Guillermo Prieto <18505468+gprietoe@users.noreply.github.com>
2022-08-26 15:53:39 -0500

    continuous vs3

--- redapy/.ipynb_checkpoints/limpieza_univar-checkpoint.py
+++ redapy/.ipynb_checkpoints/limpieza_univar-checkpoint.py
@@ -1,54 +1,37 @@
 import pandas as pd
 import numpy as np
 
+from .continuous_variables import clean_continuous_var
+from .continuous_variables import cal_intervalos
+from .continuous_variables import cal_descriptivos
+
 ### indetificar el index
 def pivot_table(df):
-    df=df.pivot(index="ubigeo",columns="vars", values="fre")
+    df=df.pivot(index="ubigeo",columns="resp", values="fre")
     return df
-
-def clean_continuous_var(df):
-    df["vars"]=df["vars"].apply(lambda x: "".join(filter(str.isnumeric, str(x)))).astype(int)
-    df["fre"]=df["fre"].astype(int)
-    ##numero
-    return df
-
-def grupos_intervalos(df,valor_inicio,intervalo):
-    df=clean_continuous_var(df)
-    
-    len_var=df.vars.max() ##valor máximo
-    list_in=list(range(valor_inicio,len_var+1,intervalo)) # lista con los intervalos 
-
-    df["vars2"]=""
-    df["lim_s"]=0
-    for p in list_in:
-        df["lim_s"]=np.where((df.vars>=p)&(df.vars<=p+intervalo),p+intervalo-1,df.lim_s)
-        df["vars2"]=np.where((df.vars>=p)&(df.vars<=p+intervalo),str(p)+"-"+str(p+intervalo-1),df.vars2)
-
-    #df["vars2"]=np.where((df.lim_s>len_var), str(max(list_in))+"-"+str(len_var-1),df.vars2)
-    del df["vars"]
-    df=df.rename({"vars2":"vars"},axis=1)
-    df=df.groupby(["ubigeo","lim_s","vars"]).sum().reset_index().sort_values("lim_s")[["ubigeo","vars","fre"]].copy()
-    return df
-
 
 ## Utilizamos la tabla resumen para crear la matriz de los valores posibles de la pregunta
 def total_cat(df):
     
-    i_start=df[df.vars=="RESUMEN"].index.values[0] #valor del index donde inicia la tabla resumen
+    i_start=df[df.resp=="RESUMEN"].index.values[0] #valor del index donde inicia la tabla resumen
     df_r=df.loc[i_start+3:]
-    i_end=df_r[df_r.vars=="Total"].index.values[0] #valor del index donde acaba la tabla resumen
+    i_end=df_r[df_r.resp=="Total"].index.values[0] #valor del index donde acaba la tabla resumen
 
     df_rf=df.loc[i_start+3:i_end-1].copy() ## tabla resumen
     return df_rf
 
 ## Crea una lista con los ubigeos de la dataframe 
 def list_ubigeo_index(df):
-    ubigeo=df[(df.vars.notna())&(df.vars.str.startswith("AREA"))]["vars"].copy() ## extrae los ubigeos de cada tabla
+    ubigeo=(df[(df.resp.notna())&
+               (df.resp.str.startswith("AREA"))]
+            ["resp"].
+            copy()
+           ) ## extrae los ubigeos de cada tabla
     list_index_d=ubigeo.index.values
     return ubigeo, list_index_d
 
 ## Función para extraer la información de las frecuencias de cada tabla de ubigeo
-def var_ubigeo(df,i_start_var):
+def extrac_freq_ubigeo(df,i_start_var):
     
     ubigeo, list_index=list_ubigeo_index(df)
     index_table=list_index[i_start_var]
@@ -57,13 +40,21 @@ def var_ubigeo(df,i_start_var):
         ubigeo_nom=q[7:]
     ###
     i_start_u=df.loc[index_table+3:]
-    i_end_u=i_start_u[(i_start_u.vars.notna())&(i_start_u.vars=="Total")]["fre"].index[0]-1
+    i_end_u=i_start_u[(i_start_u.resp.notna())&(i_start_u.resp=="Total")]["fre"].index[0]-1
     df_f=df.loc[i_start_u.index.values[0]:i_end_u].copy()
     df_f["ubigeo"]=str(ubigeo_nom)
     
     return df_f
 
-def conversion_redatam_matriz(df, continuous=False, valor_inicio=0, intervalo=None):
+def analys_cont_var(df, kind,valor_inicio,intervalo):
+    df= df.copy()
+    if kind =="intervalos":
+        df=cal_intervalos(df,valor_inicio,intervalo)
+    elif kind =="descriptivos":
+        df=cal_descriptivos(df)
+    return df
+
+def conversion_redatam_matriz(df, continuous=False, kind=None, valor_inicio=0, intervalo=None):
     """
     conversion_redatam_matriz(df: 'DataFrame') -> Dataframe
     
@@ -74,40 +65,31 @@ def conversion_redatam_matriz(df, continuous=False, valor_inicio=0, intervalo=No
 
     """
     df=df[[df.columns[1], df.columns[2]]].copy()
-    df.columns=["vars", "fre"]
+    df.columns=["resp", "fre"]
     
     df_rf=total_cat(df)                        ##categorías del resumen
-    ubigeo,list_index_f=list_ubigeo_index(df)
+    ubigeo,list_index_f=list_ubigeo_index(df)  ## Lista de ubigeos e indexes
     ubigeos_des=[]
     
     if continuous==True:
-        df_rf=(df_rf.
-               assign(ubigeo="ubigeo").
-               pipe(grupos_intervalos,valor_inicio,intervalo))
-        
-        df_rf_reindex=df_rf["vars"].tolist()
-        df_rf=pivot_table(df_rf)
-        df_rf=df_rf.drop("ubigeo")
-        
+        ## LOOP POR UBIGEO
         for p in list(range(0,len(list_index_f))): ## loop para extraer y girar los datos para cada tabla de ubigeo
             ubigeos_des.append(df.
-                               pipe(var_ubigeo,p).
-                               pipe(grupos_intervalos,valor_inicio,intervalo).## Se extra la información del ubigeo
-                               pipe(pivot_table))  ## Se gira la tabla
-        df_f=(pd.concat([df_rf, pd.concat(ubigeos_des)],axis=0).
-              fillna(0).                           ## Se reemplazan los NaN por 0, no todos lo distritos tienen todas las categorías de la variable
-              reindex(df_rf_reindex, axis=1))     
+                               pipe(extrac_freq_ubigeo,p).
+                               pipe(clean_continuous_var).
+                               pipe(analys_cont_var, kind,valor_inicio,intervalo)
+                               )
     else:
-        df_rf=(df_rf.
-               assign(ubigeo="ubigeo").
-               pipe(pivot_table))
-        df_rf=df_rf.drop("ubigeo")
+        # df_rf=(df_rf.
+        #        assign(ubigeo="ubigeo").
+        #        pipe(pivot_table))
+        # df_rf=df_rf.drop("ubigeo")
         
         for p in list(range(0,len(list_index_f))): ## loop para extraer y girar los datos para cada tabla de ubigeo
             ubigeos_des.append(df.
-                               pipe(var_ubigeo,p). ## Se extra la información del ubigeo
+                               pipe(extrac_freq_ubigeo,p). ## Se extra la información del ubigeo
                                pipe(pivot_table))  ## Se gira la tabla 
 
-        df_f=(pd.concat([df_rf, pd.concat(ubigeos_des)],axis=0).
+    df_f=(pd.concat(ubigeos_des,axis=0).
               fillna(0))
     return df_f
---
+++ redapy/continuous_variables.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import numpy as np
+
+## Función para convertir las variables continuas en integrales
+def clean_continuous_var(df):
+    df=df.copy()
+    try:
+        df=(df.
+            assign(resp=lambda df_: df_.resp.
+                   apply(lambda x:"".join(filter(str.isnumeric, str(x)))).
+                   astype(int)).
+            assign(fre=lambda df_: df_.fre.astype(int))
+           )
+    except:
+        raise AssertionError("La variable bajo análisis no es continua")
+    return df
+
+
+def cal_intervalos(df,valor_inicio,intervalo):
+    '''
+    Return a DataFrame where each column is a class interval
+    '''    
+    len_var=df.resp.max() ##valor máximo
+    list_in=(list(range(valor_inicio,len_var+1,intervalo))) # lista con los intervalos 
+
+    df["resp2"]=""
+    df["lim_s"]=0
+    for p in list_in:
+        df["lim_s"]=np.where((df.resp>=p)&(df.resp<=p+intervalo),p+intervalo-1,df.lim_s)
+        df["resp2"]=np.where((df.resp>=p)&(df.resp<=p+intervalo),str(p)+"-"+str(p+intervalo-1),df.resp2)
+
+    #df["resp2"]=np.where((df.lim_s>len_var), str(max(list_in))+"-"+str(len_var-1),df.resp2)
+    del df["resp"]
+    df=(df.
+        rename({"resp2":"resp"},axis=1).
+        groupby(["ubigeo","lim_s","resp"]).
+        sum().
+        reset_index().
+        sort_values("lim_s")
+        [["ubigeo","resp","fre"]].
+        copy().
+        pivot(index="ubigeo",columns="resp", values="fre")
+       )
+    return df
+
+def cal_descriptivos(df):
+    '''
+    Return a DataFrame with descriptive statistics
+    '''
+    df=df.copy()
+    df=(df.
+          assign(freq2=df.fre*df.resp).
+          assign(mean_2=lambda df_: df_.freq2.sum()/df_.fre.sum()).
+          assign(var1=lambda df_:((df_.resp-df_.mean_2)**2)*df_.fre)
+         )
+
+    data={"numero de casos":[],"suma":[],"maximo":[],"minimo":[],"promedio":[],"varianza":[],"des estandar":[]}
+    
+    data["numero de casos"].append(df.fre.sum())
+    data["suma"].append(df.freq2.sum())
+    data["maximo"].append(df["resp"].max())
+    data["minimo"].append(df["resp"].min())
+    data["promedio"].append(df.freq2.sum()/df.fre.sum())
+    data["varianza"].append(df.var1.sum()/df.fre.sum())
+    data["des estandar"].append(np.sqrt(df.var1.sum()/df.fre.sum()))
+    
+    df_f=(pd.DataFrame(data).
+          assign(ubigeo=df.ubigeo.iloc[0],
+                 coeficiente=lambda df_:(df_["des estandar"]/df_["promedio"])*100).
+          rename({"coeficiente":"coeficiente de variacion"},axis=1).
+          set_index("ubigeo")
+         )
+    
+    return df_f
+    
+    
